a06df58954190ddbcca2b1fe4d5329488f726130
Darlei A. Lippi <darleilippi@uri.edu.br>
2021-12-13 23:22:53 -0300

    Add routes, services and controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import "reflect-metadata";
 
 import express from "express";
 
-const server = express()
+import "./database";
+import { routes } from "./routes";
+
+const server = express();
+
+server.use(express.json());
+
+server.use(routes);
 
 server.listen(3333, () => console.log("Server is running"))
